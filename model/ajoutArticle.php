228ceaade3d015973ebb7b08febc9a97fac237da
SalahEddineMds <mm_mendas@esi.dz>
2025-02-18 15:24:26 +0100

    id_categorie

--- model/ajoutArticle.php
+++ model/ajoutArticle.php
@@ -6,13 +6,13 @@
         && !empty($_POST["date_fabrication"])
         && !empty($_POST["date_expiration"])) {
 
-        $sql = "INSERT INTO $nom_base_de_donnes.article(nom_article, id_, quantite, prix_unitaire, date_fabrication, date_expiration)
+        $sql = "INSERT INTO $nom_base_de_donnes.article(nom_article, id_categorie, quantite, prix_unitaire, date_fabrication, date_expiration)
                 VALUES (?, ?, ?, ?, ?, ?)";
             $req = $connexion->prepare($sql);
 
             $req->execute(array(
                 $_POST["nom_article"],
-                $_POST["id_"],
+                $_POST["id_categorie"],
                 $_POST["quantite"],
                 $_POST["prix_unitaire"],
                 $_POST["date_fabrication"],
