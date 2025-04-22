39c2e1ced1b0692bd3403350c790cb8aa5e93628
ign48ms <zennakiyacine23@gmail.com>
2025-04-22 10:40:09 +0100

    styling

--- vue/achat.php
+++ vue/achat.php
@@ -8,126 +8,127 @@
 
 <div class="home-content">
     <div class="overview-boxes">
+        <div class="va-container">
+            <button onclick="createNewAchat()" class="valider">Nouveau</button>
 
-        <button onclick="createNewAchat()" class="valider">Nouveau</button>
-
-        <?php if (!empty($_GET["id_achat"])): ?>
-        <div class="box">
-            <form action="../model/ajoutFournisseurAchat.php" method="post" id="achat-form">
-                <input type="hidden" name="id_achat" value="<?= $_GET['id_achat'] ?>">
-                <label for="id_fournisseur">Fournisseur</label>
-                    <select name="id_fournisseur" id="id_fournisseur">
-                    <?php 
-                        $fournisseurs = getFournisseur();
-                        if (!empty($fournisseurs) && is_array($fournisseurs)) {
-                            foreach ($fournisseurs as $key => $value) {
-                                $selected = (!empty($achat) && $achat["id_fournisseur"] == $value["id"]) ? "selected" : "";
-                                echo "<option value='{$value["id"]}' {$selected}>{$value["nom"]} {$value["prenom"]}</option>";
-                            }
-                        }
-                    ?>  
-                    </select>
-                    <button class="valider" type="submit">Valider Achat</button>
-            </form>
-            <div class="box">
-                <form action="../model/ajoutArticleAchat.php" method="post" id="article-form">
+            <?php if (!empty($_GET["id_achat"])): ?>
+            <div class="vabox">
+                <form action="../model/ajoutFournisseurAchat.php" method="post" id="achat-form">
                     <input type="hidden" name="id_achat" value="<?= $_GET['id_achat'] ?>">
-                    <label for="id_article">Article</label>
-                    <select onchange="setPrix()" name="id_article" id="id_article">
+                    <label for="id_fournisseur">Fournisseur</label>
+                        <select name="id_fournisseur" id="id_fournisseur">
                         <?php 
-                            $articles = getArticle();
-                            if (!empty($articles) && is_array($articles)) {
-                                foreach ($articles as $key => $value) {
-                                    echo "<option data-prix='{$value["prix_achat_unitaire"]}' value='{$value["id"]}'>{$value["nom_article"]} - {$value["quantite"]} disponible</option>";                                   
+                            $fournisseurs = getFournisseur();
+                            if (!empty($fournisseurs) && is_array($fournisseurs)) {
+                                foreach ($fournisseurs as $key => $value) {
+                                    $selected = (!empty($achat) && $achat["id_fournisseur"] == $value["id"]) ? "selected" : "";
+                                    echo "<option value='{$value["id"]}' {$selected}>{$value["nom"]} {$value["prenom"]}</option>";
                                 }
                             }
-                        ?> 
-                    </select>
+                        ?>  
+                        </select>
+                        <button class="valider" type="submit">Valider Achat</button>
+                </form>
+                <div class="vabox">
+                    <form action="../model/ajoutArticleAchat.php" method="post" id="article-form">
+                        <input type="hidden" name="id_achat" value="<?= $_GET['id_achat'] ?>">
+                        <label for="id_article">Article</label>
+                        <select onchange="setPrix()" name="id_article" id="id_article">
+                            <?php 
+                                $articles = getArticle();
+                                if (!empty($articles) && is_array($articles)) {
+                                    foreach ($articles as $key => $value) {
+                                        echo "<option data-prix='{$value["prix_achat_unitaire"]}' value='{$value["id"]}'>{$value["nom_article"]} - {$value["quantite"]} disponible</option>";                                   
+                                    }
+                                }
+                            ?> 
+                        </select>
 
-                    <label for="quantite">Quantité</label>
-                    <input onkeyup="setPrix()" type="number" name="quantite" id="quantite" placeholder="Veuillez saisir la quantité" min="1">
+                        <label for="quantite">Quantité</label>
+                        <input onkeyup="setPrix()" type="number" name="quantite" id="quantite" placeholder="Veuillez saisir la quantité" min="1">
 
-                    <label for="prix_u">Prix unitaire</label>
-                    <input onkeyup="setPrix()" type="number" name="prix_u" id="prix_u" placeholder="Veuillez saisir le prix unitaire" min="0" step="any">
+                        <label for="prix_u">Prix unitaire</label>
+                        <input onkeyup="setPrix()" type="number" name="prix_u" id="prix_u" placeholder="Veuillez saisir le prix unitaire" min="0" step="any">
 
-                    <label for="prix">Prix total</label>
-                    <input type="number" name="prix" id="prix" placeholder="Prix total" min="0" step="any" readonly>
+                        <label for="prix">Prix total</label>
+                        <input type="number" name="prix" id="prix" placeholder="Prix total" min="0" step="any" readonly>
 
-                    <button type="submit">Ajouter</button>
+                        <button type="submit">Ajouter</button>
 
-                    <?php
-                    if (!empty($_SESSION["message"]["text"])) {
-                    ?>
-                        <div class="alert <?=$_SESSION["message"]["type"]?>">
-                            <?=$_SESSION["message"]["text"]?>
-                        </div>
-                    <?php 
-                    unset($_SESSION["message"]); // Efface le message après affichage
-                    }
-                    ?>
-                </form>    
-            </div>                  
-        </div>
-        <?php endif; ?>
+                        <?php
+                        if (!empty($_SESSION["message"]["text"])) {
+                        ?>
+                            <div class="alert <?=$_SESSION["message"]["type"]?>">
+                                <?=$_SESSION["message"]["text"]?>
+                            </div>
+                        <?php 
+                        unset($_SESSION["message"]); // Efface le message après affichage
+                        }
+                        ?>
+                    </form>    
+                </div>                  
+            </div>
+            <?php endif; ?>
 
-        <div style="display: block;" class="box">
-            <form action="" method="get">
+            <div style="display: block;" class="box">
+                <form action="" method="get">
+                    <table class="mtable">
+                        <tr>
+                            <th>Fournisseur</th>
+                            <th>Montant</th>
+                            <th>Date</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="text" name="nom_p_fournisseur" id="nom_p_fournisseur" placeholder="Veuillez saisir le nom ou prenom">
+                            </td>
+                            <td>
+                                <input type="number" name="montant" id="montant" placeholder="Veuillez saisir le montant" min="0" step="any">
+                            </td>
+                            <td>
+                                <input type="date" name="date" id="date">
+                            </td>
+                        </tr>
+                    </table>
+                    <br>
+                    <button type="submit">Recherche</button>
+                </form>
+                <br>
+
                 <table class="mtable">
                     <tr>
                         <th>Fournisseur</th>
                         <th>Montant</th>
                         <th>Date</th>
+                        <th>Action</th>
                     </tr>
-                    <tr>
-                        <td>
-                            <input type="text" name="nom_p_fournisseur" id="nom_p_fournisseur" placeholder="Veuillez saisir le nom ou prenom">
-                        </td>
-                        <td>
-                            <input type="number" name="montant" id="montant" placeholder="Veuillez saisir le montant" min="0" step="any">
-                        </td>
-                        <td>
-                            <input type="date" name="date" id="date">
-                        </td>
-                    </tr>
-                </table>
-                <br>
-                <button type="submit">Recherche</button>
-            </form>
-            <br>
+                    <?php
+                    if (!empty($_GET)) {
+                        $achats = getAchat(null, $_GET);
+                    } else {
+                        $achats = getAchat();
+                    }
+                    if (!empty($achats) && is_array( $achats )) {
+                        foreach ($achats as $key => $value) {
+                            if ($value["total"] > 0) {
+                        ?>
+                        <tr>
+                            <td><?=$value["nom"]. " ".$value["prenom"] ?></td>
+                            <td><?=number_format($value["total"],2,".","")?></td>
+                            <td><?=date("d/m/Y H:i:s", strtotime($value["date_achat"]))?></td>
+                            <td>
+                                <a href="recuAchat.php?id=<?= $value["id"]?>" style="color: blue !important;"><i class='bx bx-receipt'></i></a>
+                                <a onclick="annuleAchat(<?= $value['id']?>) "style="color: red; cursor: pointer;"><i class='bx bx-x-circle'></i></a>
+                            </td>
 
-            <table class="mtable">
-                <tr>
-                    <th>Fournisseur</th>
-                    <th>Montant</th>
-                    <th>Date</th>
-                    <th>Action</th>
-                </tr>
-                <?php
-                if (!empty($_GET)) {
-                    $achats = getAchat(null, $_GET);
-                } else {
-                    $achats = getAchat();
-                }
-                if (!empty($achats) && is_array( $achats )) {
-                    foreach ($achats as $key => $value) {
-                        if ($value["total"] > 0) {
-                    ?>
-                    <tr>
-                        <td><?=$value["nom"]. " ".$value["prenom"] ?></td>
-                        <td><?=number_format($value["total"],2,".","")?></td>
-                        <td><?=date("d/m/Y H:i:s", strtotime($value["date_achat"]))?></td>
-                        <td>
-                            <a href="recuAchat.php?id=<?= $value["id"]?>" style="color: blue !important;"><i class='bx bx-receipt'></i></a>
-                            <a onclick="annuleAchat(<?= $value['id']?>) "style="color: red; cursor: pointer;"><i class='bx bx-x-circle'></i></a>
-                        </td>
-
-                    </tr>    
-                <?php
+                        </tr>    
+                    <?php
+                            }
                         }
                     }
-                }
-                ?>
-            </table>
+                    ?>
+                </table>
+            </div>
         </div>
     </div>
 </div>
--- vue/vente.php
+++ vue/vente.php
@@ -8,130 +8,131 @@
 
 <div class="home-content">
     <div class="overview-boxes">
-        
-        <button onclick="createNewVente()" class="valider">Nouveau</button>
+        <div class="va-container">
+            <button onclick="createNewVente()" class="valider">Nouveau Vente</button>
 
-        <?php if (!empty($_GET["id_vente"])): ?>
-        <div class="box">
-            <form action="../model/ajoutClientVente.php" method="post" id="vente-form">
-                <input type="hidden" name="id_vente" value="<?= $_GET['id_vente'] ?>">
-                <label for="id_client">Client</label>
-                <select name="id_client" id="id_client">
-                    <?php 
-                        $clients = getClient();
-                        if (!empty($clients) && is_array($clients)) {
-                            foreach ($clients as $key => $value) {
-                                $selected = (!empty($vente) && $vente["id_client"] == $value["id"]) ? "selected" : "";
-                                echo "<option value='{$value["id"]}' {$selected}>{$value["nom"]} {$value["prenom"]}</option>";
-                            }
-                        }
-                    ?>  
-                </select>
-                <button class="valider" type="submit">Valider Vente</button>
-            </form>
-
-            <div class="box">
-                <form action="../model/ajoutArticleVente.php" method="post" id="article-form">
+            <?php if (!empty($_GET["id_vente"])): ?>
+            <div class="vabox">
+                <form action="../model/ajoutClientVente.php" method="post" id="vente-form">
                     <input type="hidden" name="id_vente" value="<?= $_GET['id_vente'] ?>">
-                    <label for="id_article">Article</label>
-                    <select onchange="remplirPrix()" name="id_article" id="id_article">
-                        <option value="" disabled selected>Choisir un article</option>
+                    <label for="id_client">Client</label>
+                    <select name="id_client" id="id_client">
                         <?php 
-                            $articles = getArticle();
-                            if (!empty($articles) && is_array($articles)) {
-                                foreach ($articles as $key => $value) {
-                                    echo "<option data-prix='{$value["prix_vente_unitaire"]}' value='{$value["id"]}'>{$value["nom_article"]} - {$value["quantite"]} disponible</option>";                                   
+                            $clients = getClient();
+                            if (!empty($clients) && is_array($clients)) {
+                                foreach ($clients as $key => $value) {
+                                    $selected = (!empty($vente) && $vente["id_client"] == $value["id"]) ? "selected" : "";
+                                    echo "<option value='{$value["id"]}' {$selected}>{$value["nom"]} {$value["prenom"]}</option>";
                                 }
                             }
                         ?>  
                     </select>
-
-                    <label for="quantite">Quantité</label>
-                    <input onkeyup="setPrix()" type="number" name="quantite" id="quantite" placeholder="Veuillez saisir la quantité" min="1">
-
-                    <label for="prix_u">Prix unitaire</label>
-                    <input onkeyup="setPrix()" type="number" name="prix_u" id="prix_u" placeholder="Prix unitaire" min="0" step="any">
-                    
-                    <label for="prix">Prix total</label>
-                    <input type="number" name="prix" id="prix" placeholder="Veuillez saisir le prix" min="0" step="any">
-                    
-
-                    <button type="submit">Ajouter</button>
-                    <?php
-                    if (!empty($_SESSION["message"]["text"])) {
-                    ?>
-                        <div class="alert <?=$_SESSION["message"]["type"]?>">
-                            <?=$_SESSION["message"]["text"]?>
-                        </div>
-                    <?php 
-                    unset($_SESSION["message"]); // Efface le message après affichage
-                    }
-                    ?>
+                    <button class="valider" type="submit">Valider Vente</button>
                 </form>
+
+                <div class="vabox">
+                    <form action="../model/ajoutArticleVente.php" method="post" id="article-form">
+                        <input type="hidden" name="id_vente" value="<?= $_GET['id_vente'] ?>">
+                        <label for="id_article">Article</label>
+                        <select onchange="remplirPrix()" name="id_article" id="id_article">
+                            <option value="" disabled selected>Choisir un article</option>
+                            <?php 
+                                $articles = getArticle();
+                                if (!empty($articles) && is_array($articles)) {
+                                    foreach ($articles as $key => $value) {
+                                        echo "<option data-prix='{$value["prix_vente_unitaire"]}' value='{$value["id"]}'>{$value["nom_article"]} - {$value["quantite"]} disponible</option>";                                   
+                                    }
+                                }
+                            ?>  
+                        </select>
+
+                        <label for="quantite">Quantité</label>
+                        <input onkeyup="setPrix()" type="number" name="quantite" id="quantite" placeholder="Veuillez saisir la quantité" min="1">
+
+                        <label for="prix_u">Prix unitaire</label>
+                        <input onkeyup="setPrix()" type="number" name="prix_u" id="prix_u" placeholder="Prix unitaire" min="0" step="any">
+                        
+                        <label for="prix">Prix total</label>
+                        <input type="number" name="prix" id="prix" placeholder="Veuillez saisir le prix" min="0" step="any">
+                        
+
+                        <button type="submit">Ajouter</button>
+                        <?php
+                        if (!empty($_SESSION["message"]["text"])) {
+                        ?>
+                            <div class="alert <?=$_SESSION["message"]["type"]?>">
+                                <?=$_SESSION["message"]["text"]?>
+                            </div>
+                        <?php 
+                        unset($_SESSION["message"]); // Efface le message après affichage
+                        }
+                        ?>
+                    </form>
+                </div>
             </div>
-        </div>
-        <?php endif; ?>
+            <?php endif; ?>
 
 
-        <div style="display: block;" class="box">
-            <form action="" method="get">
+            <div style="display: block;" class="box">
+                <form action="" method="get">
+                    <table class="mtable">
+                        <tr>
+                            <th>Client</th>
+                            <th>Montant</th>
+                            <th>Date</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <input type="text" name="nom_p_client" id="nom_p_client" placeholder="Veuillez saisir le nom ou prenom">
+                            </td>
+                            <td>
+                                <input type="number" name="montant" id="montant" placeholder="Veuillez saisir le montant" min="0" step="any">
+                            </td>
+                            <td>
+                                <input type="date" name="date" id="date">
+                            </td>
+                        </tr>
+                    </table>
+                    <br>
+                    <button type="submit">Recherche</button>
+                </form>
+                <br>
+                        
                 <table class="mtable">
                     <tr>
                         <th>Client</th>
                         <th>Montant</th>
                         <th>Date</th>
+                        <th>Action</th>
                     </tr>
-                    <tr>
-                        <td>
-                            <input type="text" name="nom_p_client" id="nom_p_client" placeholder="Veuillez saisir le nom ou prenom">
-                        </td>
-                        <td>
-                            <input type="number" name="montant" id="montant" placeholder="Veuillez saisir le montant" min="0" step="any">
-                        </td>
-                        <td>
-                            <input type="date" name="date" id="date">
-                        </td>
-                    </tr>
-                </table>
-                <br>
-                <button type="submit">Recherche</button>
-            </form>
-            <br>
-                    
-            <table class="mtable">
-                <tr>
-                    <th>Client</th>
-                    <th>Montant</th>
-                    <th>Date</th>
-                    <th>Action</th>
-                </tr>
-                <?php
-                if (!empty($_GET)) {
-                    $ventes = getVente(null, $_GET);
-                } else {
-                    $ventes = getVente();
-                }
-                if (!empty($ventes) && is_array( $ventes )) {
-                    foreach ($ventes as $key => $value) {
-                        if ($value["total"] > 0) {
-                    ?>
-                    <tr>                      
-                        <td><?=$value["nom"]. " ".$value["prenom"] ?></td>
-                        <td><?= $value["total"] ?></td>
-                        <td><?=date("d/m/Y H:i:s", strtotime($value["date_vente"]))?></td>
-                        <td>
-                            <a href="recuVente.php?id=<?= $value["id"]?>" title="Afficher le Reçu" style="color: blue !important;"><i class='bx bx-receipt'></i></a>
-                            <a href="vente.php?id_vente=<?=$value['id']?>" title="Ajouter à Vente" style="color: blue !important;"><i class='bx bx-edit-alt'></i></a>
-                            <a onclick="annuleVente(<?= $value['id']?>)" title="Supprimer cette Vente" style="color: red; cursor: pointer;"><i class='bx bx-x-circle'></i></a>
-                        </td>
+                    <?php
+                    if (!empty($_GET)) {
+                        $ventes = getVente(null, $_GET);
+                    } else {
+                        $ventes = getVente();
+                    }
+                    if (!empty($ventes) && is_array( $ventes )) {
+                        foreach ($ventes as $key => $value) {
+                            if ($value["total"] > 0) {
+                        ?>
+                        <tr>                      
+                            <td><?=$value["nom"]. " ".$value["prenom"] ?></td>
+                            <td><?= $value["total"] ?></td>
+                            <td><?=date("d/m/Y H:i:s", strtotime($value["date_vente"]))?></td>
+                            <td>
+                                <a href="recuVente.php?id=<?= $value["id"]?>" title="Afficher le Reçu" style="color: blue !important;"><i class='bx bx-receipt'></i></a>
+                                <a href="vente.php?id_vente=<?=$value['id']?>" title="Ajouter à Vente" style="color: blue !important;"><i class='bx bx-edit-alt'></i></a>
+                                <a onclick="annuleVente(<?= $value['id']?>)" title="Supprimer cette Vente" style="color: red; cursor: pointer;"><i class='bx bx-x-circle'></i></a>
+                            </td>
 
-                    </tr>    
-                <?php
+                        </tr>    
+                    <?php
+                            }
                         }
                     }
-                }
-                ?>
-            </table>
+                    ?>
+                </table>
+            </div>
         </div>
     </div>
 </div>
